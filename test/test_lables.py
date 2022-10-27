3bd38598d4be3f40eb8785500344bcd8bd1bc9ea
Helen <dudarevalen_1997@mail.ru>
2022-10-27 15:49:41 +0500

    -m

--- test/test_lables.py
+++ test/test_lables.py
@@ -1,29 +1,16 @@
 import allure
-from selene.support import by
-from selene.support.conditions import be
+from allure_commons.types import Severity
+from selene import by
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
 
-def test_dynamic_github():
-    with allure.step('Открываем главную страницу'):
-        browser.open("https://github.com")
-
-    with allure.step('Ищем репозиторий'):
-        s(".header-search-input").click()
-        s(".header-search-input").send_keys("eroshenkoam/allure-example")
-        s(".header-search-input").submit()
-
-    with allure.step('Переходим по ссылке репозитория'):
-        s(by.link_text("eroshenkoam/allure-example")).click()
-
-    with allure.step('Открывает таб issues'):
-        s("#issues-tab").click()
-
-    with allure.step('Проверяем наличие issues с номером 81'):
-        s(by.partial_text("#81")).should(be.visible)
-
-
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'eroshenkoam')
+@allure.feature('Задачи в репозитоории')
+@allure.story('Авторизованный пользователь может создать задачу в репозитории')
+@allure.link('https://github.com', name='Testing')
 def test_decorator_steps():
     open_main_page()
     search_for_repository("eroshenkoam/allure-example")
@@ -36,15 +23,18 @@ def test_decorator_steps():
 def open_main_page():
     browser.open('https://github.com')
 
+
 @allure.step('Ищем репозитория {repo}')
 def search_for_repository(repo):
     s(".header-search-input").click()
     s(".header-search-input").send_keys(repo)
     s(".header-search-input").submit()
 
+
 @allure.step('Переходим по ссылке репозитория {repo}')
 def go_to_repository(repo):
     s(by.link_text("eroshenkoam/allure-example")).click()
+
 
 @allure.step('Открываем таб Issues')
 def open_issue_tab():
