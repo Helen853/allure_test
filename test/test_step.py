4cbbdb81d9afedb0860b55eae4cb48d8af03e833
Helen <dudarevalen_1997@mail.ru>
2022-10-27 14:34:41 +0500

    -m

--- test/test_step.py
+++ test/test_step.py
@@ -22,3 +22,36 @@ def test_dynamic_github():
 
     with allure.step('Проверяем наличие issues с номером 81'):
         s(by.partial_text("#81")).should(be.visible)
+
+
+def steps_decorator_steps():
+    open_main_page()
+    search_for_repository("eroshenkoam/allure-example")
+    go_to_repository("eroshenkoam/allure-example")
+    open_issue_tab()
+    should_see_issue_with_number("#81")
+
+
+@allure.step('Открываем декоратор')
+def open_main_page():
+    browser.open('https://github.com')
+
+@allure.step('Ищем репозитория {repo}')
+def search_for_repository(repo):
+    s(".header-search-input").click()
+    s(".header-search-input").send_keys(repo)
+    s(".header-search-input").submit()
+
+@allure.step('Переходим по ссылке репозитория {repo}')
+def go_to_repository(repo):
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+@allure.step('Открываем таб Issues')
+def open_issue_tab():
+    s("#issues-tab").click()
+
+
+@allure.step('Проверяем наличие Issue с номером {number}')
+def should_see_issue_with_number(number):
+    s(by.partial_text(number)).click()
+
